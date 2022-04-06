ba229c80d5d2ebff9ac0e592e132af2a46c91396
pioggia73 <monikawasilewska13@wp.pl>
2022-04-06 12:46:49 +0200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+        <h1 className='section-title'>about us</h1>
+        <p>
+        I'm baby microdosing try-hard pug butcher pinterest fashion axe. DIY try-hard plaid raclette unicorn meditation fanny pack semiotics crucifix hexagon vape. Tattooed selfies af mlkshk. Sustainable farm-to-table pork belly kogi organic deep v. Mlkshk bicycle rights tbh 3 wolf moon, chia skateboard thundercats street art celiac lyft cloud bread locavore. Tbh enamel pin direct trade copper mug craft beer before they sold out irony, selfies swag hashtag pok pok williamsburg edison bulb lumbersexual pinterest. Gastropub gluten-free PBR&B selfies brooklyn DIY blog.
+        </p>
+    </section>
+  )
+}
+
+export default About
